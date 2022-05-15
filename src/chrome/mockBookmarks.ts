96cc2898fa3f5b85fd67c1c0a4de13c9492fc2c3
Alex Eshtokin <alexeshtokin@machine.local>
2022-05-15 15:04:53 +0300

    add mock data for bookmarks tree

---
+++ src/chrome/mockBookmarks.ts
@@ -0,0 +1,101 @@
+const BOOKMARKS_TREE: chrome.bookmarks.BookmarkTreeNode = {
+  children: [
+    {
+      children: [
+        {
+          children: [
+            {
+              children: [
+                {
+                  children: [
+                    {
+                      dateAdded: 1652035364322,
+                      id: '732',
+                      index: 0,
+                      parentId: '734',
+                      title:
+                        'ROZETKA | Компьютерный стол E-Table Universal с регулируемой высотой Черный (3201B). Цена, купить Компьютерный стол E-Table Universal с регулируемой высотой Черный (3201B) в Киеве, Харькове, Днепре, Одессе, Запорожье, Львове. Компьютерный стол E-Table Universal с регулируемой высотой Черный (3201B): обзор, описание, продажа',
+                      url: 'https://rozetka.com.ua/e_table_3201b/p249394646/',
+                    },
+                  ],
+                  dateAdded: 1652035399326,
+                  dateGroupModified: 1652035403102,
+                  id: '734',
+                  index: 0,
+                  parentId: '733',
+                  title: 'programming',
+                },
+              ],
+              dateAdded: 1652035399326,
+              dateGroupModified: 1652035399326,
+              id: '733',
+              index: 4,
+              parentId: '330',
+              title: 'Shopping',
+            },
+          ],
+          dateAdded: 1648397214801,
+          dateGroupModified: 1649052892248,
+          id: '330',
+          index: 1,
+          parentId: '1',
+          title: 'Brave',
+        },
+        {
+          dateAdded: 1650999943083,
+          id: '722',
+          index: 4,
+          parentId: '1',
+          title:
+            '[MAN-7512] [My address] When user edit address, text overflow when input is long - Jira',
+          url: 'https://makroo2o.atlassian.net/browse/MAN-7512',
+        },
+      ],
+      dateAdded: 1652536167166,
+      dateGroupModified: 1652536167166,
+      id: '1',
+      index: 0,
+      parentId: '0',
+      title: 'Bookmarks bar',
+    },
+    {
+      children: [
+        {
+          dateAdded: 1650568372898,
+          id: '709',
+          index: 1,
+          parentId: '2',
+          title: 'Samurices NFT - Storyline',
+          url: 'https://www.samurices.xyz/storyline',
+        },
+        {
+          dateAdded: 1650724407502,
+          id: '710',
+          index: 2,
+          parentId: '2',
+          title: 'Таблица символов Юникода (Unicode) - Хитрые инструменты',
+          url: 'http://foxtools.ru/Unicode',
+        },
+        {
+          dateAdded: 1650724573241,
+          id: '711',
+          index: 3,
+          parentId: '2',
+          title: 'RegExr: Learn, Build, & Test RegEx',
+          url: 'https://regexr.com/',
+        },
+      ],
+      dateAdded: 1648194468765,
+      dateGroupModified: 1650742692486,
+      id: '2',
+      index: 1,
+      parentId: '0',
+      title: 'Other Bookmarks',
+    },
+  ],
+  dateAdded: 1652536167166,
+  id: '0',
+  title: '',
+}
+
+export default BOOKMARKS_TREE
